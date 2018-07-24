2ca28df3ac8a06f340bb76af067d61fb9db618e5
Edjânio <edjanio.upe@gmail.com>
2018-07-24 20:49:31 -0300

    Criação do CrudDAO genérico

---
+++ projeto-jsf1/src/java/br/edu/ifpe/usuarios/dao/CrudDao.java
@@ -0,0 +1,15 @@
+
+package br.edu.ifpe.usuarios.dao;
+
+import br.edu.ifpe.usuarios.util.exception.ErroSistema;
+import java.util.List;
+
+public interface CrudDao<E> {
+    
+    public void salvar(E entidade)throws ErroSistema;
+    public void deletar(E entidade)throws ErroSistema;
+    public List<E>listar();
+    
+    
+    
+}
