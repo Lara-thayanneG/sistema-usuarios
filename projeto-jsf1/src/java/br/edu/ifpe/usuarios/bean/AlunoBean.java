a219af7791928bd5f74308f46b4d2c5d80c4bae3
Edjânio <edjanio.upe@gmail.com>
2018-08-28 21:01:33 -0300

    Criação de AlunoBean e função de Inserção na página Gerenciar-alunos

---
+++ projeto-jsf1/src/java/br/edu/ifpe/usuarios/bean/AlunoBean.java
@@ -0,0 +1,31 @@
+
+package br.edu.ifpe.usuarios.bean;
+
+import br.edu.ifpe.usuarios.entidades.Aluno;
+import br.edu.ifpe.usuarios.dao.AlunoDao;
+import br.edu.ifpe.usuarios.util.exception.ErroSistema;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+
+@ManagedBean
+@SessionScoped
+public class AlunoBean {
+    
+    private Aluno aluno = new Aluno(null,"","","","","");
+    
+    public void adicionar() throws ErroSistema{
+        new AlunoDao().salvar(aluno);
+    }
+
+    public Aluno getAluno() {
+        return aluno;
+    }
+
+    public void setAluno(Aluno aluno) {
+        this.aluno = aluno;
+    }
+    
+    
+    
+}
